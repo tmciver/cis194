1224026fe35f58da81e17a87a68ce3f3e7faf47b
Tim McIver <tim@timmciver.com>
2015-10-11 22:54:13 -0400

    Removed unnecessary pattern match in definition of `e` function.

--- src/Homework3/Golf.hs
+++ src/Homework3/Golf.hs
@@ -4,7 +4,6 @@ module Homework3.Golf
 
 skips :: [a] -> [[a]]
 skips l = [e n l | n <- [1..length l]]
-  where e _ [] = []
-        e n l = case drop (n-1) l of
+  where e n l = case drop (n-1) l of
           [] -> []
           (x:xs) -> x : e n xs
